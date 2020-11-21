dfce010a74cb7680524c810738c6dcce1cba7e27
Jeff Johnson <synotik@hotmail.com>
2020-11-21 06:56:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,9 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(names)
-
+  rooms = []
+  names.each_with_index do |name, index|
+    rooms << "Hello, #{name}! You'll be assigned to room " (index + 1)
+  end
+  return rooms
 end
